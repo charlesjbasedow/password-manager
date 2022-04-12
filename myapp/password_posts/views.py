3e50b38c33ff3962296a8602b5687afd9a327f89
charlesjbasedow <cbasedow@outlook.com>
2022-04-12 04:10:05 -0400

    update password post route create

--- myapp/password_posts/views.py
+++ myapp/password_posts/views.py
@@ -17,7 +17,7 @@ def create_post():
         flash('Password Post was Created')
         print('Password Post was created')
         return redirect(url_for('core.index'))
-    return render_template('create_password_post.html', form=form)
+    return render_template('create_post.html', form=form)
 
 # Make sure the password_post_id is an integer!
 
@@ -25,3 +25,26 @@ def create_post():
 def password_post(password_post_id):
     password_post = PasswordVault.query.get_or_404(password_post_id) 
     return render_template('password_post.html', title=password_post.title, date=password_post.date, post=password_post)
+
+@password_posts.route('/<int:password_post_id>/update',methods=['GET','POST'])
+@login_required
+def update(password_post_id):
+    password_post = PasswordVault.query.get_or_404(password_post_id)
+
+    if password_post.author != current_user:
+        abort(403)
+
+    form = PasswordPostForm()
+
+    if form.validate_on_submit():
+        password_post.title = form.title.data
+        password_post.text = form.text.data
+        db.session.commit()
+        flash('password Post Updated')
+        return redirect(url_for('password_posts.password_post',password_post_id=password_post.id))
+
+    elif request.method == 'GET':
+        form.title.data = password_post.title
+        form.text.data = password_post.text
+
+    return render_template('create_post.html',title='Updating',form=form)
