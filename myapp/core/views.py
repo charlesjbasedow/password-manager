7adc8f006417077ed5ee3075f771945ea16d46a3
charlesjbasedow <cbasedow@outlook.com>
2022-04-14 17:19:17 -0400

    more styling

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -6,5 +6,5 @@ core = Blueprint('core', __name__)
 @core.route('/')
 def index():
     page = request.args.get('page', 1, type=int)
-    password_posts = PasswordVault.query.order_by(PasswordVault.date.desc()).paginate(page=page, per_page=5)
+    password_posts = PasswordVault.query.order_by(PasswordVault.date.desc()).paginate(page=page, per_page=100)
     return render_template('index.html', password_posts=password_posts)
