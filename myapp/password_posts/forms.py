d71a290cc8ae45d96704efa55ee7e58587783085
charlesjbasedow <cbasedow@outlook.com>
2022-04-14 18:20:55 -0400

    add more styling

--- myapp/password_posts/forms.py
+++ myapp/password_posts/forms.py
@@ -6,4 +6,4 @@ class PasswordPostForm(FlaskForm):
     company = StringField('Company', validators=[DataRequired()])
     user_name = StringField('Username', validators=[DataRequired()])
     pword = StringField('Password', validators=[DataRequired()])
-    submit = SubmitField('Create')
+    submit = SubmitField('Submit')
